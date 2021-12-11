3580944ab3c13733784e93490c7a549865b3765a
Ernesto Fernandez <pro.ernesto.fernandez@gmail.com>
2021-12-11 03:13:48 -0300

    Template front 2

--- services/query_service.js
+++ services/query_service.js
@@ -1,9 +1,9 @@
 var mongoose = require('mongoose');
-require("../models/query");
+require("../models/queries");
 require("../models/user");
-// const item = require("../models/item");
-// const vehicle = require("../models/vehicle");
-// const item_query = require("../models/item_query");
+const item = require("../models/item");
+const vehicle = require("../models/vehicle");
+const item_query = require("../models/item_query");
 var Query = mongoose.model('query');
 var ItemQuery = mongoose.model('item_query');
 var User = mongoose.model('user');
