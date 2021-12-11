f9cff4514a11dfd842407a3807d4958bc2c67f05
Ernesto Fernandez <pro.ernesto.fernandez@gmail.com>
2021-12-11 02:56:07 -0300

    Template front 2

--- loaders/mongoose.js
+++ loaders/mongoose.js
@@ -2,15 +2,8 @@
 require('dotenv').config()
 
 var mongoose = require('mongoose');
-const user = require("../models/user");
-const query = require("../models/query");
-const item = require("../models/item");
-const vehicle = require("../models/vehicle");
-const item_query = require("../models/item_query");
-
-
 const mongoDB_path = process.env.MONGODB_PATH;
-console.log(mongoDB_path)
+
 mongoose.connect(mongoDB_path, function (err, res) {
     if (err) {
         console.log('ERROR: No se pudo conectar con: ' + mongoDB_path + '. ' + err);
@@ -18,3 +11,12 @@ mongoose.connect(mongoDB_path, function (err, res) {
         console.log('Connectado a la base de datos: ' + mongoDB_path);
     }
 });
+
+const user = require("../models/user");
+const query = require("../models/query");
+const item = require("../models/item");
+const vehicle = require("../models/vehicle");
+const item_query = require("../models/item_query");
+
+
+
