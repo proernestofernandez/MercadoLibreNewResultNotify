a4f16660aa93e5030b62a6dee8ea25efce00f6d6
Ernesto Fernandez <pro.ernesto.fernandez@gmail.com>
2021-12-19 18:22:51 -0300

    Uncommenting cron

--- cron/cronExecuteQueries.js
+++ cron/cronExecuteQueries.js
@@ -3,5 +3,5 @@ const cron = require('node-cron');
 
 cron.schedule('0 0,30 * * * *', function () {
   console.log('Ejecutando todas las consultas cada media hora');
-  //queries_service.execute_all_queries_from_db();
+  queries_service.execute_all_queries_from_db();
 });
